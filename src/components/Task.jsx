c6577760f474a4c610c1c046e6d2387e8ab3b072
Kelio <keliojosue@outlook.com>
2022-12-27 13:08:28 -0300

    Show date in common tasks

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -42,7 +42,12 @@ export default function Task(props) {
         <img src={`${imgName}.png`} className="task-type-img" />
         <div className="task-info" onClick={props.openTask}>
           <p className="task-name">{props.name}</p>
-          {props.reminder && <p className="task-time">{props.time}</p>}
+          {props.reminder && (
+            <p className="task-time">
+              <span>{props.time}</span>{" "}
+              {!props.recurring && <span>{props.date.split("-").reverse().join(" / ")}</span>}
+            </p>
+          )}
         </div>
         <div style={{ flexGrow: 1 }} />
         <img src={`${props.status}.png`} className="task-status-img" onClick={props.changeStatus} />
